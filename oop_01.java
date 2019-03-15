783ae5c14a2b6b546070bff5c59f6572fec729f7
Pivotal <pcf-redis@pivotal.io>
2019-03-15 17:05:32 +0000

    First OOP commit

---
+++ oop_01.java
@@ -0,0 +1,12 @@
+public class Account {
+
+    // a noargs constructor
+    public Account() {
+        balance = 0.00;
+    }
+    // contstructor with args
+    public Account(float value) {
+        this.balance = value;
+    }
+
+}
